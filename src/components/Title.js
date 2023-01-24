37024434528c3ab7b84f227ad46b19df731c8b1a
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-01-24 15:41:30 -0300

    Requisito 3 ok

---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{ headline }</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: 'Planetas',
+};
+
+export default Title;
